439def9cb198cbb1ab19742e2c43da9ae9bc4539
padysmike <padys93@gmail.com>
2021-08-31 15:23:43 +0200

    Add of help methods

--- CodingChalenge1/CodingChalenge1/Program.cs
+++ CodingChalenge1/CodingChalenge1/Program.cs
@@ -1,40 +1,55 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
 
 namespace CodingChalenge1
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
             var config = ConfigLoader.LoadConfig();
 
-            var textInput = ReadText(config.InputFilePath);
-
-            WriteText(config.OutputFilePath, textInput);
+            StartWithFile(config.InputFilePath, config.OutputFilePath);
         }
 
-        private static void WriteText(string outputTextPath, string textOutput)
+        private static void StartWithFile(string inputTextPath, string outputTextPath)
         {
             using (var streamWriter = new StreamWriter(outputTextPath))
             {
-                streamWriter.Write(textOutput);
+                using (var streamReader = new StreamReader(inputTextPath))
+                {
+                    StartWithStreams(streamWriter, streamReader);
+                    streamReader.Close();
+                }
                 streamWriter.Flush();
                 streamWriter.Close();
             }
         }
 
-        private static string ReadText(string inputTextPath)
+        private static void StartWithStreams(StreamWriter streamWriter, StreamReader streamReader)
         {
-            string textInput;
-            using (var streamReader = new StreamReader(inputTextPath))
-            {
-                textInput = streamReader.ReadToEnd();
-                streamReader.Close();
-            }
+            WriteText(streamWriter);
+        }
 
-            return textInput;
+
+        private static void WriteText(StreamWriter streamWriter)
+        {
+            streamWriter.WriteLine("text");
+        }
+
+        private static string[] ReadWords(StreamReader streamReader)
+        {
+            var line = streamReader.ReadLine().Trim().ToLower();
+            return line.Split(" ");
+        }
+
+        private static int ReadNumber(StreamReader streamReader)
+        {
+            var testCaseCountRaw = streamReader.ReadLine();
+            return int.Parse(testCaseCountRaw.Trim());
         }
     }
 }
